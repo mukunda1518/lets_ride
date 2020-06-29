139675f0815b5f4119f07be23c61e4d340b0bbb2
mukunda <mukundapeddamekala123@gmail.com>
2020-06-29 15:27:41 +0000

    adds base chages in lets_ride

--- lets_ride/interactors/presenters/presenter_interface.py
+++ lets_ride/interactors/presenters/presenter_interface.py
@@ -12,26 +12,6 @@ from lets_ride.dtos.dtos import (
 class PresenterInterface(ABC):
 
     @abstractmethod
-    def get_sign_up_response(self, token_dto: UserAuthTokensDTO):
-        pass
-
-    @abstractmethod
-    def get_login_response(self, token_dto: UserAuthTokensDTO):
-        pass
-
-    @abstractmethod
-    def user_profile_response(self, user_dto: UserDto):
-        pass
-
-    @abstractmethod
-    def raise_invalid_phone_number(self):
-        pass
-
-    @abstractmethod
-    def raise_invalid_password(self):
-        pass
-
-    @abstractmethod
     def get_ride_requests_response(self, ride_requests_dto: RideRequestsDto):
         pass
 
@@ -46,12 +26,4 @@ class PresenterInterface(ABC):
         self,
         ride_asset_matching_dto: RideAssetMatchingDto
     ):
-        pass
-
-    @abstractmethod
-    def raise_username_already_exist(self):
-        pass
-
-    @abstractmethod
-    def raise_user_with_phone_number_already_exist(self):
         pass
--- lets_ride/presenters/presenter_implementation.py
+++ lets_ride/presenters/presenter_implementation.py
@@ -4,14 +4,9 @@ from django_swagger_utils.drf_server.exceptions \
     import NotFound, Forbidden, BadRequest
 from lets_ride.interactors.presenters.presenter_interface \
     import PresenterInterface
-from lets_ride.dtos.dtos import UserDto
-from common.dtos import UserAuthTokensDTO
+
 from lets_ride.constants.exception_messages import (
-    INVALID_PHONE_NUMBER,
-    INVALID_PASSWORD,
-    INVALID_OFFSET_LIMIT_VALUE,
-    INVALID_USERNAME,
-    PHONE_NUMBER_WITH_USER_EXIST
+    INVALID_OFFSET_LIMIT_VALUE
 )
 from lets_ride.dtos.dtos import (
     RideRequestDto,
@@ -34,52 +29,9 @@ from lets_ride.constants.constants import DEFAULT_DATE_TIME_FORMAT
 
 class PresenterImplementation(PresenterInterface):
 
-    def get_sign_up_response(self, token_dto: UserAuthTokensDTO):
-        response_dict = {
-            "user_id": token_dto.user_id,
-            "access_token": token_dto.access_token,
-            "refresh_token": token_dto.refresh_token,
-            "expires_in": token_dto.expires_in
-        }
-        return response_dict
-
-
-    def get_login_response(self, token_dto: UserAuthTokensDTO):
-        response_dict = {
-            "user_id": token_dto.user_id,
-            "access_token": token_dto.access_token,
-            "refresh_token": token_dto.refresh_token,
-            "expires_in": token_dto.expires_in
-        }
-        return response_dict
-
-
-    def user_profile_response(self, user_dto: UserDto):
-        user_dict = {
-            "username": user_dto.username,
-            "phone_number": user_dto.phone_number,
-            "profile_pic_url": user_dto.profile_pic
-        }
-        return user_dict
-
-    def raise_invalid_phone_number(self):
-        raise NotFound(*INVALID_PHONE_NUMBER)
-
-
-    def raise_invalid_password(self):
-        raise NotFound(*INVALID_PASSWORD)
-
 
     def raise_invalid_for_limit_and_offset(self):
         raise BadRequest(*INVALID_OFFSET_LIMIT_VALUE)
-
-
-    def raise_username_already_exist(self):
-        raise BadRequest(*INVALID_USERNAME)
-
-
-    def raise_user_with_phone_number_already_exist(self):
-        raise BadRequest(*PHONE_NUMBER_WITH_USER_EXIST)
 
 
     def  get_asset_requests_response(
