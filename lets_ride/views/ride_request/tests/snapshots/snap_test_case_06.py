8491d77e5c7184cea3c065073547e0a1941cd683
mukunda <mukundapeddamekala123@gmail.com>
2020-07-02 06:17:47 +0000

    adds contoller test cases for my ride requests apis

---
+++ lets_ride/views/ride_request/tests/snapshots/snap_test_case_06.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+
+snapshots = Snapshot()
+
+snapshots['TestCase06RideRequestAPITestCase::test_case status'] = 400
+
+snapshots['TestCase06RideRequestAPITestCase::test_case body'] = {
+    'http_status_code': 400,
+    'res_status': 'INVALID_DATE_TIME',
+    'response': 'travelling datetime should be greater than current datetime'
+}
+
+snapshots['TestCase06RideRequestAPITestCase::test_case header_params'] = {
+    'content-language': [
+        'Content-Language',
+        'en'
+    ],
+    'content-length': [
+        '135',
+        'Content-Length'
+    ],
+    'content-type': [
+        'Content-Type',
+        'text/html; charset=utf-8'
+    ],
+    'vary': [
+        'Accept-Language, Origin',
+        'Vary'
+    ],
+    'x-frame-options': [
+        'DENY',
+        'X-Frame-Options'
+    ]
+}
